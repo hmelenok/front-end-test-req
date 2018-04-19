e03114fa77be695a64d3dabfd7b7a2d3c64141f0
Andrii <anjey101@gmail.com>
2018-04-19 22:07:17 +0300

    fetching data refactored to simplier object form

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Table from './components/Table/Table';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Table from "./components/Table/Table";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true,
-      groups: []
-    }
+      searchTerm: "",
+      data: []
+    };
   }
   componentDidMount() {
-    this.fetchData()
-  }
-  fetchData() {
-    //getting only the fields that are needed
-    fetch('/groups-summary.json')
-        .then(response => response.json())
-        .then(data => data.data.map(group => (
-          {
-            title: `${group.title}`,
-            owner: `${group.ownerUsername}`,
-            creationDate: `${group.createdAt}`,
-            members: `${group.membersCount}`,
-            gems: `${group.gemsCount}`
-          }
-        )))
-        .then(groups => this.setState({
-          groups,
-          isLoading: false
-        }))
-        .catch(error => console.log('parsing failed', error))
+    window
+      .fetch("/groups-summary.json")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          data: data.map(group => ({
+            title: group.title,
+            owner: group.ownerUsername,
+            creationDate: group.createdAt,
+            members: group.membersCount,
+            gems: group.gemsCount
+          }))
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   render() {
+    const { searchTerm, data } = this.state;
+
+    const columns = [
+      {
+        name: "Title",
+        key: "title"
+      },
+      {
+        name: "Owner",
+        key: "owner"
+      },
+      {
+        name: "CreationDate",
+        key: "creationDate"
+      },
+      {
+        name: "Members",
+        key: "members"
+      },
+      {
+        name: "Gems",
+        key: "gems"
+      }
+    ];
+
     return (
-      <div className="App">
-        <Table />
+      <div>
+        <Table columns={columns} />
       </div>
     );
   }
