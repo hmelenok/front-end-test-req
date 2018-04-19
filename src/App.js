3d2368698d907774fe405f10b9e3c2bb31f0f0d9
Andrii <andriy-kost1992@ukr.net>
2018-04-19 13:30:35 +0300

    fetch request updated

--- src/App.js
+++ src/App.js
@@ -7,11 +7,32 @@ import Table from './components/Table/Table';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoading: true,
+      groups: []
+    }
   }
   componentDidMount() {
+    this.fetchData()
+  }
+  fetchData() {
+    //getting only the fields that are needed
     fetch('/groups-summary.json')
-        .then(response => console.log(response.json()))
-        .catch(error => error)
+        .then(response => response.json())
+        .then(data => data.data.map(group => (
+          {
+            title: `${group.title}`,
+            owner: `${group.ownerUsername}`,
+            creationDate: `${group.createdAt}`,
+            members: `${group.membersCount}`,
+            gems: `${group.gemsCount}`
+          }
+        )))
+        .then(groups => this.setState({
+          groups,
+          isLoading: false
+        }))
+        .catch(error => console.log('parsing failed', error))
   }
   render() {
     return (
